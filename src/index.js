40467df18200c771be2aeae04f141dda91a0c513
saninoleg <96084949+saninoleg@users.noreply.github.com>
2022-01-11 01:56:28 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse(n) {
     return 
-	parseFloat(n.toString().
-	split("").
-	reverse().
-	join(""));
+	parseFloat(n.toString()
+	.split('')
+	.reverse()
+	.join(''));
 };
